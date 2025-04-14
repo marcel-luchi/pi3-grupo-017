ff6bfd41bea3d639690e274be0aa822d434b70b4
miria2413 <miria.meus.cursos@gmail.com>
2025-04-14 08:33:08 -0300

    previsao do tempo

--- javascri.js
+++ javascri.js
@@ -75,3 +75,62 @@ uploadPhoto.addEventListener('change', () => {
     };
     reader.readAsDataURL(uploadPhoto.files[0]);
 });
+
+
+//previsao do clima ///////////////////////////////////
+const key = "64d6a970c4878f69bc82ea7739b9fcfa";
+
+
+function colocarDadosNaTela(dados){
+    document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name;
+
+    document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "° C" + "  agora";
+
+    document.querySelector(".max-min").innerHTML = "Max.: " + Math.floor(dados.main.temp_max) + " / " + "Min.: " + Math.floor(dados.main.temp_min);
+
+    document.querySelector(".texto-previsao").innerHTML = dados.weather[0].description;
+
+    document.querySelector(".umidade").innerHTML = "Umidade: " + dados.main.humidity + "%" + " - " + "Vento: " + dados.wind.speed + "km/h";
+
+    document.querySelector(".icone").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`;
+}
+
+
+
+async function buscarCidade(cidade) {
+
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then(resposta => resposta.json())
+    
+    colocarDadosNaTela(dados)
+}
+
+function cliqueiNoBotao() {
+    const cidade = document.querySelector(".input-cidade").value
+
+    buscarCidade(cidade)
+}
+
+
+
+
+//document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name;
+
+//document = html
+//queryselector = selecionar algo no html
+// (".cidade") = a class selecionada do html
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// VARIÁVEIS => Um espaço da memória do computador que guardamos algo (um numero, uma letra, um texto, uma imagem)
+// FUNÇÃO => Um trecho de código que só é executado quando é chamado
